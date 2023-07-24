22f6ceb81b9e4f11a0ae64df3158b3a4217b5008
sslatif <saqiblateef223@gmail.com>
2023-07-24 13:01:19 +0500

    Get complete address from latlng in android

--- android/app/src/main/java/com/sample/MyLocationService.java
+++ android/app/src/main/java/com/sample/MyLocationService.java
@@ -4,6 +4,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.IBinder;
@@ -16,6 +18,9 @@ import com.sample.locations.ApiClient;
 import com.sample.locations.AppExecutors;
 import com.sample.locations.LocationHelper;
 import com.sample.locations.LocationResponse;
+
+import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -60,13 +65,25 @@ public class MyLocationService extends Service {
         locationHelper.startListeningUserLocation(this, location -> {
             mLocation = location;
             if (mLocation != null) {
-                Toast.makeText(
-                        getApplicationContext(),
-                        "Location: " + location.getLatitude() + "," + location.getLongitude(),
-                        Toast.LENGTH_SHORT
-                ).show();
                 Log.d(TAG, "onLocationChanged: Latitude " + mLocation.getLatitude() +
                         " , Longitude " + mLocation.getLongitude());
+
+                Geocoder geocoder;
+                List<Address> addresses;
+                geocoder = new Geocoder(this, Locale.getDefault());
+
+                try {
+                    addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                    String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                    String city = addresses.get(0).getLocality();
+                    String state = addresses.get(0).getAdminArea();
+                    String country = addresses.get(0).getCountryName();
+                    String postalCode = addresses.get(0).getPostalCode();
+                    String knownName = addresses.get(0).getFeatureName();
+                    Log.d("Address", "Address:" + address + ", City:" + city + ",State:" + state + ",Country:" + country);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
                 AppExecutors.getInstance().networkIO().execute(() -> {
                     ApiClient apiClient = ApiClient.Companion.getInstance(MyLocationService.this)
@@ -75,7 +92,7 @@ public class MyLocationService extends Service {
                     responseCall.enqueue(new Callback<LocationResponse>() {
                         @Override
                         public void onResponse(Call<LocationResponse> call, Response<LocationResponse> response) {
-                            Log.d(TAG, "run: Running = Location Update Successful");
+                            //Log.d(TAG, "run: Running = Location Update Successful");
                         }
 
                         @Override
